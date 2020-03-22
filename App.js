fd944af40aa29eb5de99e91274882407eda6b34a
sai-sruthi <saishruti95@gmail.com>
2020-03-22 15:28:37 -0400

    new branch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Weather Forecast!! Hi</Text>
+      <Text>Welcome to Weather Forecast!!</Text>
     </View>
   );
 }
