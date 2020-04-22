2c76971263158a6c68f542612a6f192088efa687
Sanket <sgpai2@ncsu.edu>
2020-04-22 15:21:53 -0400

    Modify Forecast functionality

--- app/containers/forecast.js
+++ app/containers/forecast.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import ForecastInfo from '../components/forecastInfo';
 import styles from '../styles';
+import {searchForecastByCity} from '../services/forecastService';
+import SearchBox from '../components/searchBox';
+
+
+// function useForceUpdate() {
+//     console.log("fd");
+//     let [value, setState] = useState(true);
+//     return () => setState(!value);
+//     }
+
+let forecastData = {};
+
+const sf = async (q) =>{
+    forecastData = await searchForecastByCity(q);
+    console.log(forecastData);
+}
 
 export default function Forecast (props) {
-
-    const forecastData = props.route.params.forecastData;
-
+    forecastData = props.route.params.forecastData;
+    
+    
+    // console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    // console.log(forecastData);
     return (
-        <View style={styles.mainContainer}>
-            <Text>{forecastData.city.name}</Text>
+        <View style={styles.container}>
+            <SearchBox onComplete={sf} />
+            {/* <ForecastInfo
+                forecastData= {forecastData}
+            /> */}
+            <Text>{forecastData[5].name}</Text> 
         </View>
+        
     );
 
 };
--- app/services/forecastService.js
+++ app/services/forecastService.js
@@ -3,6 +3,7 @@ import config from '../../config';
 
 export const searchForecastByCoordinates = async (latitude, longitude) => {
     let forecastData = {};
+    let dailyData = {};
     await axios.get(config.forecast_url, {
         params: {
             lat: latitude,
@@ -10,18 +11,29 @@ export const searchForecastByCoordinates = async (latitude, longitude) => {
             appid: config.appid,
         }
     })
+
     .then(function (response) {
         forecastData = response.data;
-        // console.log(response.data);
+        dailyData = forecastData.list.filter(reading => {   
+        return reading.dt_txt.includes("18:00:00")
+        
+    })
+    
     })
     .catch(function (error) {
         console.log("Error retreiving forecast data for current location");
     });
-    return forecastData;
+    dailyData.push(forecastData.city);
+    // console.log("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+    // console.log(dailyData);
+    return dailyData;
+    
 };
 
-export const searchForecastByCity = (searchTerm) => {
-    axios.get(config.forecast_url, {
+export const searchForecastByCity = async (searchTerm) => {
+    let forecastData = {};
+    let dailyData = {};
+    await axios.get(config.forecast_url, {
         params: {
             q: searchTerm,
             appid: config.appid,
@@ -29,12 +41,15 @@ export const searchForecastByCity = (searchTerm) => {
     })
     .then(function (response) {
         forecastData = response.data;
-        // console.log(response.data);
+        dailyData = forecastData.list.filter(reading => {   
+            return reading.dt_txt.includes("18:00:00")
+            
+        })
     })
     .catch(function (error) {
         console.log("Error retreiving forecast data for entered city");
     })
-    .finally(function () {
-        return forecastData;
-    });
+    dailyData.push(forecastData.city);
+    // console.log(dailyData);
+    return dailyData;
 };
